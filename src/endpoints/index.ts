8352283942baa9f7089b31185fd377207e27a632
edlingao <sonido244@gmail.com>
2022-09-10 11:29:30 -0500

    fix(): adding env support

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -1,4 +1,4 @@
-export const baseURL = 'http://localhost:8080';
+export const baseURL = `${import.meta.env.VITE_API}`;
 
 export const register = `${baseURL}/register`;
 export const loginRoute = `${baseURL}/login`;
